930ac612e18ee78869f65a94e8807d909295b009
everzet <ever.zet@gmail.com>
2013-02-10 04:11:00 +0100

    Install mysql-server too

--- rc/manifests/classes/mysql.pp
+++ rc/manifests/classes/mysql.pp
@@ -1,7 +1,17 @@
 class mysql {
+  package { 'mysql-server':
+    ensure  => present,
+    require => Exec['apt-update'],
+  }
+
   package { 'php5-mysql':
     ensure  => present,
     require => Package['php5'],
     notify  => Service['apache2'],
+  }
+
+  service { 'mysql':
+    ensure  => running,
+    require => Package['mysql-server'],
   }
 }
