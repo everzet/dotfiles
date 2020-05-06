fcf3610a9ae037f06e65a60baeb06f327dc59dbe
everzet <ever.zet@gmail.com>
2020-05-06 12:26:41 +0100

    Re-export and cleanup terminal settings

--- shell/envars.zsh
+++ shell/envars.zsh
@@ -3,12 +3,14 @@ export SHELL='zsh'
 export EDITOR='vim'
 export GIT_EDITOR='vim'
 
-# term
-export TERM='xterm'
-
-# terminal colors
+# enable terminal colors
+export TERM='xterm-256color'
 export CLICOLOR=true
-export LSCOLORS='exfxcxdxbxegedabagacad'
+export CLICOLOR_FORCE=true
+
+# congirue LS colors (generated with https://geoff.greer.fm/lscolors/)
+export LSCOLORS='ExGxBxDxCxEgEdxbxgxcxd'
+export LS_COLORS='di=1;34:ln=1;36:so=1;31:pi=1;33:ex=1;32:bd=1;34;46:cd=1;34;43:su=0;41:sg=0;46:tw=0;42:ow=0;43'
 
 # encoding
 export LANG=en_GB.UTF-8
