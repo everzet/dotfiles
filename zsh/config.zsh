2142874105cf271a80c4016e64ae3f028c9205df
everzet <ever.zet@gmail.com>
2020-06-02 14:07:55 +0100

    Enable vim mode

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -111,9 +111,30 @@ setopt HIST_IGNORE_ALL_DUPS
 setopt HIST_IGNORE_SPACE
 setopt HIST_EXPIRE_DUPS_FIRST
 
-# keybindings
-bindkey '^f' forward-word
-bindkey '^b' backward-word
+# vim keybindings
+bindkey -v
+export KEYTIMEOUT=1
+
+# change cursor shape for different vi modes.
+function zle-keymap-select {
+  if [[ ${KEYMAP} == vicmd ]] ||
+     [[ $1 = 'block' ]]; then
+    echo -ne '\e[1 q'
+  elif [[ ${KEYMAP} == main ]] ||
+       [[ ${KEYMAP} == viins ]] ||
+       [[ ${KEYMAP} = '' ]] ||
+       [[ $1 = 'beam' ]]; then
+    echo -ne '\e[5 q'
+  fi
+}
+zle -N zle-keymap-select
+zle-line-init() {
+    zle -K viins # initiate `vi insert` as keymap (can be removed if `bindkey -V` has been set elsewhere)
+    echo -ne "\e[5 q"
+}
+zle -N zle-line-init
+echo -ne '\e[5 q' # Use beam shape cursor on startup.
+preexec() { echo -ne '\e[5 q' ;} # Use beam shape cursor for each new prompt.
 
 # Last command timer if command took longer than 5 seconds
 function preexec() { timer=${timer:-$SECONDS} }
