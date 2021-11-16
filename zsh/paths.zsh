d9ea438ef4c48a354d11ba3f09ecad9a3d3f0d35
everzet <ever.zet@gmail.com>
2021-11-16 21:20:11 +0000

    Fix PATH under TMUX

--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -1,6 +1,11 @@
-export PATH=/usr/local/sbin:$PATH
+# This fixes /usr/bin being prepended
+# to PATH when under TMUX
+if [ -f /etc/profile ]; then
+    PATH=""
+    source /etc/profile
+fi
+
 export PATH=/usr/local/bin:$PATH
-export PATH=~/.asdf/shims:$PATH
 export PATH=./node_modules/.bin:$PATH
 export PATH=./vendor/bin:$PATH
 export PATH=./bin:$PATH
