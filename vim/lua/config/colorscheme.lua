d27b7ce66cfc629c275b9842eaad839845a0ccd6
everzet <ever.zet@gmail.com>
2021-12-30 12:46:38 +0000

    line numbers are dimmed a bit in vim

--- vim/lua/config/colorscheme.lua
+++ vim/lua/config/colorscheme.lua
@@ -6,6 +6,7 @@ opt.termguicolors = true
 -- Dracula Morbius
 cmd 'colorscheme dracula_pro_morbius'
 cmd 'highlight Comment guifg=#A97079 gui=italic'
+cmd 'highlight LineNr guifg=#584145'
 
 -- Normal Dracula Pro
 --cmd 'colorscheme dracula_pro'
