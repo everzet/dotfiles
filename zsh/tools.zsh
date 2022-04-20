3bf54f2f821176f1ca9ca5425e1f69e94b03308b
Konstantin Kudryashov <konstantin.kudryashov@birdie.care>
2022-04-20 16:14:43 +0100

    add NVM and RVM settings to zshrc

--- zsh/tools.zsh
+++ zsh/tools.zsh
@@ -34,6 +34,13 @@ if [ -f ~/.fzf.zsh ]; then
         export FZF_CTRL_T_COMMAND=$FZF_DEFAULT_COMMAND
 fi
 
+# NVM
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
+# RVM
+source ~/.rvm/scripts/rvm
+
 # Direnv
 eval "$(direnv hook zsh)"
 
