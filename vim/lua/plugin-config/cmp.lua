6366e73c37be56c39cd3604a513e24358fb05eca
everzet <ever.zet@gmail.com>
2022-04-22 09:18:06 +0100

    fix c-n and c-p mappings for autocompletion

--- vim/lua/plugin-config/cmp.lua
+++ vim/lua/plugin-config/cmp.lua
@@ -16,6 +16,8 @@ cmp.setup({
       i = cmp.mapping.abort(),
       c = cmp.mapping.close(),
     }),
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-n>'] = cmp.mapping.select_next_item(),
     ['<C-space>'] = cmp.mapping.complete(),
     ['<Tab>'] = cmp.config.disable,
     ['<S-Tab>'] = cmp.config.disable,
