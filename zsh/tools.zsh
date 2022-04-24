68acccd52046f6d0b3b9c6e308cc6642c29b99ee
everzet <ever.zet@gmail.com>
2022-04-24 21:09:39 +0100

    optimise tools

--- zsh/tools.zsh
+++ zsh/tools.zsh
@@ -1,51 +1,25 @@
-# iTerm2 shell integration
-if [ -f ~/.iterm2_shell_integration.zsh ]; then
-        source ~/.iterm2_shell_integration.zsh
-fi
-
 # Homebrew
 if [ -f /opt/homebrew/bin/brew ]; then
-        eval "$(/opt/homebrew/bin/brew shellenv)"
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
-
-# ASDF version manager
-if [ -f ~/.asdf/asdf.sh ]; then
-        source ~/.asdf/asdf.sh
-        export PATH=~/.asdf/shims:$PATH
-elif [ -f /usr/local/opt/asdf/asdf.sh ]; then
-        source /usr/local/opt/asdf/asdf.sh
-        export PATH=~/.asdf/shims:$PATH
-fi
-
-# SDKMan version manager
-if [ -f ~/.sdkman/bin/sdkman-init.sh ]; then
-        source ~/.sdkman/bin/sdkman-init.sh
-fi
-
-# Flutter
-if [ -f /usr/local/share/flutter/bin/flutter ]; then
-        export PATH=/usr/local/share/flutter/bin:$PATH
-fi
-
-# FZF autocompleter
-if [ -f ~/.fzf.zsh ]; then
-        source ~/.fzf.zsh
-        export FZF_DEFAULT_COMMAND='rg --files --ignore --hidden --follow --glob "!.git/*"'
-        export FZF_CTRL_T_COMMAND=$FZF_DEFAULT_COMMAND
-fi
-
-# NVM
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-
-# RVM
-source ~/.rvm/scripts/rvm
-
-# Dotnet
-export DOTNET_ROOT="/opt/homebrew/opt/dotnet/libexec"
 
 # Direnv
 eval "$(direnv hook zsh)"
+
+# ASDF version manager
+if [ -f /opt/homebrew/opt/asdf/libexec/asdf.sh ]; then
+  . /opt/homebrew/opt/asdf/libexec/asdf.sh
+fi
+
+# FZF autocompleter
+if [ -f "${HOME}/.fzf.zsh" ]; then
+   . "${HOME}/.fzf.zsh"
+   export FZF_DEFAULT_COMMAND='rg --files --ignore --hidden --follow --glob "!.git/*"'
+   export FZF_CTRL_T_COMMAND=$FZF_DEFAULT_COMMAND
+fi
+
+# Dotnet
+export DOTNET_ROOT="/opt/homebrew/opt/dotnet/libexec"
 
 # GPG
 export GPG_TTY=$(tty)
