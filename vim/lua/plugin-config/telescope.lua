0b743a2437ef295b56e5d99d86c35f5d1e0d3669
everzet <ever.zet@gmail.com>
2022-05-02 10:47:05 +0100

    configure relative and absolute file browsers

--- vim/lua/plugin-config/telescope.lua
+++ vim/lua/plugin-config/telescope.lua
@@ -5,13 +5,8 @@ local map = vim.api.nvim_set_keymap
 map('n', '<c-t>', '<cmd>Telescope find_files<cr>', {noremap = true})
 map('n', '<c-g>', '<cmd>Telescope live_grep<cr>', {noremap = true})
 map('n', '<c-b>', '<cmd>Telescope buffers<cr>', {noremap = true})
-
-map(
-  'n',
-  '<c-f>',
-  "<cmd>lua require 'telescope'.extensions.file_browser.file_browser()<cr>",
-  {noremap = true}
-)
+map('n', '<c-r>', "<cmd>Telescope file_browser path=%:p:h<cr>", {noremap = true})
+map('n', '<c-f>', "<cmd>Telescope file_browser<cr>", {noremap = true})
 
 -- Lookup files under the neovim config folder
 map('n', '<leader>ve', ':Telescope find_files cwd=~/.config/nvim/<cr>', {noremap = true})
