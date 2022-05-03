db4f151b00470186a10960ad8862bfe8f1d85e35
everzet <ever.zet@gmail.com>
2022-05-03 12:42:15 +0100

    update telescope mappings

--- vim/lua/plugin-config/telescope.lua
+++ vim/lua/plugin-config/telescope.lua
@@ -3,10 +3,10 @@ require('telescope').load_extension('file_browser')
 local map = vim.api.nvim_set_keymap
 
 map('n', '<c-t>', '<cmd>Telescope find_files<cr>', {noremap = true})
-map('n', '<c-g>', '<cmd>Telescope live_grep<cr>', {noremap = true})
-map('n', '<c-b>', '<cmd>Telescope buffers<cr>', {noremap = true})
-map('n', '<c-r>', "<cmd>Telescope file_browser path=%:p:h<cr>", {noremap = true})
-map('n', '<c-f>', "<cmd>Telescope file_browser<cr>", {noremap = true})
+map('n', '<leader>gb', '<cmd>Telescope live_grep<cr>', {noremap = true})
+map('n', '<leader>bb', '<cmd>Telescope buffers<cr>', {noremap = true})
+map('n', '<leader>fr', "<cmd>Telescope file_browser path=%:p:h<cr>", {noremap = true})
+map('n', '<leader>fb', "<cmd>Telescope file_browser<cr>", {noremap = true})
 
 -- Lookup files under the neovim config folder
 map('n', '<leader>ve', ':Telescope find_files cwd=~/.config/nvim/<cr>', {noremap = true})
