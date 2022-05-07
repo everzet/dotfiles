73067da7f8e325e7661b86699c2b9d113f065849
everzet <ever.zet@gmail.com>
2022-05-07 12:07:13 +0100

    add mapping for showing line diagnostic

--- vim/lua/plugin-config/lspconfig/init.lua
+++ vim/lua/plugin-config/lspconfig/init.lua
@@ -17,6 +17,7 @@ local on_attach = function(_, bufnr)
   buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
   buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
   buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+  buf_set_keymap('n', '<leader>sd', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
 
 end
 
