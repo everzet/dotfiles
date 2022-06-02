433ab488e7a24a0823638cc5e508ea8b1e2fccf1
everzet <ever.zet@gmail.com>
2022-06-02 11:04:42 +0100

    switch colorscheme to normal Dracula Pro

--- vim/lua/config/colorscheme.lua
+++ vim/lua/config/colorscheme.lua
@@ -3,11 +3,5 @@ local cmd = vim.cmd
 
 opt.termguicolors = true
 
--- Dracula Morbius
-cmd 'colorscheme dracula_pro_morbius'
-cmd 'highlight Comment guifg=#A97079 gui=italic'
-cmd 'highlight LineNr guifg=#584145'
-
--- Normal Dracula Pro
---cmd 'colorscheme dracula_pro'
---cmd 'highlight Comment guifg=#7970A9 gui=italic'
+cmd 'colorscheme dracula_pro'
+cmd 'highlight Comment guifg=#7970A9 gui=italic'
