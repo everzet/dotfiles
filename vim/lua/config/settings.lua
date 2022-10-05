129b08c70453512183911012cbcccc61cf1d5794
Konstantin Kudryashov <konstantin.kudryashov@birdie.care>
2022-10-05 10:29:39 +0100

    hide last status and command history (neovim 0.8+)

--- vim/lua/config/settings.lua
+++ vim/lua/config/settings.lua
@@ -19,6 +19,8 @@ opt.splitright = true
 opt.visualbell = true
 opt.mouse = 'a'
 opt.list = true
+opt.ch = 0
+opt.ls = 0
 opt.listchars = { tab = '▸ ', trail = '·' }
 opt.backup = false
 opt.swapfile = false
