ad69515e6fc5accd1b584d1dac6b81bc177594a0
Konstantin Kudryashov <konstantin.kudryashov@birdie.care>
2022-10-05 10:47:57 +0100

    optimise tools

--- zsh/tools.zsh
+++ zsh/tools.zsh
@@ -12,11 +12,22 @@ if [ -f /opt/homebrew/opt/asdf/libexec/asdf.sh ]; then
 fi
 
 # FZF autocompleter
-if [ -f "${HOME}/.fzf.zsh" ]; then
-   . "${HOME}/.fzf.zsh"
-   export FZF_DEFAULT_COMMAND='rg --files --ignore --hidden --follow --glob "!.git/*"'
-   export FZF_CTRL_T_COMMAND=$FZF_DEFAULT_COMMAND
-fi
+export FZF_DEFAULT_COMMAND='rg --files --ignore --hidden --follow --glob "!.git/*"'
+export FZF_CTRL_T_COMMAND=$FZF_DEFAULT_COMMAND
+[ -s "${HOME}/.fzf.zsh" ] && \. "${HOME}/.fzf.zsh"
+
+# NVM
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+# RVM
+export RVM_DIR="$HOME/.rvm"
+[ -s $RVM_DIR ] && export PATH="$PATH:$RVM_DIR/bin"
+
+# PNPM
+export PNPM_HOME="$HOME/Library/pnpm"
+[ -s $PNPM_HOME ] && export PATH="$PATH:$PNPM_HOME"
 
 # Dotnet
 export DOTNET_ROOT="/opt/homebrew/opt/dotnet/libexec"
