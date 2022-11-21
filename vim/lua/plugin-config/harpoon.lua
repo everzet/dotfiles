19b50e638f0cd5586fe9a79cbc8f6e0fd96f02b9
Konstantin Kudryashov <konstantin.kudryashov@birdie.care>
2022-11-21 10:41:05 +0000

    finetune Harpoon window width

--- vim/lua/plugin-config/harpoon.lua
+++ vim/lua/plugin-config/harpoon.lua
@@ -1,10 +1,8 @@
 local map = vim.api.nvim_set_keymap
 
-require("harpoon").setup({
-  menu = {
-    width = math.floor(vim.api.nvim_win_get_width(0) * 0.8),
-  }
-})
+require("harpoon").setup {
+  menu = { width = 80 }
+}
 
 map('n', '<leader>la', '<cmd>:lua require("harpoon.mark").add_file()<cr>', { noremap = true })
 map('n', '<leader>ll', '<cmd>:lua require("harpoon.ui").toggle_quick_menu()<cr>', { noremap = true })
