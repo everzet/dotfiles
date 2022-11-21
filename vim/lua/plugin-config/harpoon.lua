2bbb7eb17a1df43b165dc7fc1136d40242b2d9b0
Konstantin Kudryashov <konstantin.kudryashov@birdie.care>
2022-11-21 09:52:32 +0000

    make Harpoon window ~80% of the Vim window

--- vim/lua/plugin-config/harpoon.lua
+++ vim/lua/plugin-config/harpoon.lua
@@ -1,9 +1,9 @@
 local map = vim.api.nvim_set_keymap
 
 require("harpoon").setup({
-    menu = {
-        width = vim.api.nvim_win_get_width(0) - 4,
-    }
+  menu = {
+    width = math.floor(vim.api.nvim_win_get_width(0) * 0.8),
+  }
 })
 
 map('n', '<leader>la', '<cmd>:lua require("harpoon.mark").add_file()<cr>', { noremap = true })
