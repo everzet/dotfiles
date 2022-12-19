7c13e2e880fde583abe7424438803445c8aa59bf
everzet <ever.zet@gmail.com>
2022-12-19 11:17:30 +0000

    rework lsp configu, fix vim global warnings in config

--- vim/after/plugin/lsp.lua
+++ vim/after/plugin/lsp.lua
@@ -1,21 +1,44 @@
 local lsp = require('lsp-zero')
 local cmp = require('cmp')
 
-local servers = {
+-- Use recommended preset
+lsp.preset('recommended')
+
+-- Always install following servers
+lsp.ensure_installed({
     'elixirls',
     'tsserver',
     'eslint',
     'sumneko_lua',
-}
-local preferences = {}
-local cmp_select = { behavior = cmp.SelectBehavior.Select }
-local cmp_mappings = lsp.defaults.cmp_mappings({
-    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-    ['<C-Space>'] = cmp.mapping.complete(),
 })
-local on_attach = function(_, bufnr)
+
+-- Fix Undefined global 'vim'
+lsp.configure('sumneko_lua', {
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { 'vim' }
+            }
+        }
+    }
+})
+
+-- Setup completion keybinds
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
+lsp.setup_nvim_cmp({
+    mapping = lsp.defaults.cmp_mappings({
+        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+        ['<C-Space>'] = cmp.mapping.complete(),
+    })
+})
+
+-- Override default preferences
+lsp.set_preferences({})
+
+-- Set keybinds on LSP attach to the buffer
+lsp.on_attach(function(_, bufnr)
     local opts = { buffer = bufnr, remap = false }
 
     vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
@@ -29,11 +52,6 @@ local on_attach = function(_, bufnr)
     vim.keymap.set('n', '<leader>lrr', function() vim.lsp.buf.references() end, opts)
     vim.keymap.set('n', '<leader>lrn', function() vim.lsp.buf.rename() end, opts)
     vim.keymap.set('i', '<C-h>', function() vim.lsp.buf.signature_help() end, opts)
-end
+end)
 
-lsp.preset('recommended')
-lsp.ensure_installed(servers)
-lsp.set_preferences(preferences)
-lsp.setup_nvim_cmp({ mapping = cmp_mappings })
-lsp.on_attach(on_attach)
 lsp.setup()
