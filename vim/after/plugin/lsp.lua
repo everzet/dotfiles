1933ac7c914a099381af0bc42bbaba6d18563ac8
everzet <ever.zet@gmail.com>
2022-12-19 12:05:52 +0000

    further configure Lua LSP to work with vim config

--- vim/after/plugin/lsp.lua
+++ vim/after/plugin/lsp.lua
@@ -12,13 +12,23 @@ lsp.ensure_installed({
     'sumneko_lua',
 })
 
--- Fix Undefined global 'vim'
+-- Configure Lua LSP to support vim configs
+local runtime_path = vim.split(package.path, ';')
+table.insert(runtime_path, 'lua/?.lua')
+table.insert(runtime_path, 'lua/?/init.lua')
 lsp.configure('sumneko_lua', {
     settings = {
         Lua = {
-            diagnostics = {
-                globals = { 'vim' }
-            }
+            runtime = {
+                -- Tell the language server which version of Lua you're using (most likely LuaJIT)
+                version = 'LuaJIT',
+                -- Setup your lua path
+                path = runtime_path,
+            },
+            diagnostics = { globals = { 'vim' } },
+            workspace = { library = vim.api.nvim_get_runtime_file('', true) },
+            -- Do not send telemetry data containing a randomized but unique identifier
+            telemetry = { enable = false },
         }
     }
 })
