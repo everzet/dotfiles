8625531baf446af03539f63c1aa7a1316868a2d1
everzet <ever.zet@gmail.com>
2022-12-19 11:52:26 +0000

    adopt `f` ("find") mnemonic for Telescope shortcuts

--- vim/after/plugin/telescope.lua
+++ vim/after/plugin/telescope.lua
@@ -3,16 +3,16 @@ telescope.load_extension('fzf')
 telescope.load_extension('file_browser')
 
 local builtin = require('telescope.builtin')
-local fb = require('telescope').extensions.file_browser
+local fb = telescope.extensions.file_browser
 
 vim.keymap.set('n', '<C-t>', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>gb', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb',
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>ff',
     function() fb.file_browser({ select_buffer = true, initial_mode = 'normal' }) end, {})
 vim.keymap.set('n', '<leader>fr',
     function() fb.file_browser({ path = '%:p:h', select_buffer = true, initial_mode = 'normal' }) end, {})
-vim.keymap.set('n', '<leader>bb',
+vim.keymap.set('n', '<leader>fb',
     function() builtin.buffers({ initial_mode = 'normal' }) end, {})
 
 -- Lookup files under the neovim config folder
