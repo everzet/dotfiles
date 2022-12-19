f7076807d8ca4ffee85286bec540e354b5918e35
everzet <ever.zet@gmail.com>
2022-12-19 12:06:51 +0000

    automatically install packer on first vim boot

--- vim/lua/plugins.lua
+++ vim/lua/plugins.lua
@@ -1,3 +1,10 @@
+-- Install packer
+local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
+  vim.cmd [[packadd packer.nvim]]
+end
+
 return require('packer').startup(function(use)
     -- Package manager
     use 'wbthomason/packer.nvim'
