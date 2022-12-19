aab385f6318deac01d2bfef19ece8d012a53b6ba
everzet <ever.zet@gmail.com>
2022-12-19 12:27:02 +0000

    disable Lua's Treesitter for the time being

--- vim/after/plugin/treesitter.lua
+++ vim/after/plugin/treesitter.lua
@@ -22,6 +22,9 @@ require('nvim-treesitter.configs').setup {
         -- `false` will disable the whole extension
         enable = true,
 
+        -- Lua parser is borked at the moment (19.12.22) :(
+        disable = { 'lua' },
+
         -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
         -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
         -- Using this option may slow down your editor, and you may see some duplicate highlights.
