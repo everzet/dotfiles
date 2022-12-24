d62608893b5cd6ef6fe91cbd1d7fc063c848d249
everzet <ever.zet@gmail.com>
2022-12-24 17:00:51 +0000

    cleanup paths

--- fish/conf.d/paths.fish
+++ fish/conf.d/paths.fish
@@ -1,6 +1,3 @@
 fish_add_path /usr/local/bin
-fish_add_path "/Applications/Sublime Text.app/Contents/SharedSupport/bin"
-fish_add_path ~/.mix/escripts
 fish_add_path ./node_modules/.bin
-fish_add_path ./vendor/bin
 fish_add_path ./bin
