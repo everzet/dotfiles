940e3e6192702aa83b21701d989fb6a93aa17943
everzet <ever.zet@gmail.com>
2022-12-26 23:15:58 +0000

    rework colors to avoid warnings

--- vim/lua/colors.lua
+++ vim/lua/colors.lua
@@ -1,27 +1,28 @@
-M = {}
+Colors = {}
 
-function M.setup()
+function Colors.setup()
+    -- Configure colorscheme
     vim.opt.termguicolors = true
     vim.cmd 'colorscheme dracula_pro'
 
     -- Italicise comments
-    M.update_hl('Comment', { italic = true })
+    Colors.update_hl('Comment', { italic = true })
 end
 
-function M.get_hl(name)
+function Colors.get_hl(name)
     return vim.api.nvim_get_hl_by_name(name, true)
 end
 
-function M.update_hl(name, overrides)
+function Colors.update_hl(name, overrides)
     local hl = vim.api.nvim_get_hl_by_name(name, true)
     for k, v in pairs(overrides) do hl[k] = v end
     vim.api.nvim_set_hl(0, name, hl)
 end
 
-function M.inherit_hl(from_name, to_name, overrides)
+function Colors.inherit_hl(from_name, to_name, overrides)
     local hl = vim.api.nvim_get_hl_by_name(from_name, true)
     for k, v in pairs(overrides) do hl[k] = v end
     vim.api.nvim_set_hl(0, to_name, hl)
 end
 
-return M
+return Colors
