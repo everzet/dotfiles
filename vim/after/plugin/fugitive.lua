18972581c76fbe8a2d15f8bc8195adde9d49ecc6
everzet <ever.zet@gmail.com>
2022-12-27 15:26:14 +0000

    add quicker shortcuts for staging and commit

--- vim/after/plugin/fugitive.lua
+++ vim/after/plugin/fugitive.lua
@@ -1,4 +1,6 @@
 vim.keymap.set('n', '<leader>gs', vim.cmd.Git, { remap = false })
+vim.keymap.set('n', '<leader>ga', vim.cmd.Gwrite, { remap = false })
+vim.keymap.set('n', '<leader>gc', function() vim.cmd.Git('commit') end, { remap = false })
 
 local augroup = vim.api.nvim_create_augroup('Everzet_Fugitive', { clear = true })
 vim.api.nvim_create_autocmd('BufWinEnter', {
@@ -9,7 +11,6 @@ vim.api.nvim_create_autocmd('BufWinEnter', {
         local bufnr = vim.api.nvim_get_current_buf()
         local opts = { buffer = bufnr, remap = false }
 
-        vim.keymap.set('n', '<leader>gc', function() vim.cmd.Git('commit') end, opts)
         vim.keymap.set('n', '<leader>gP', function() vim.cmd.Git({ 'pull', '--rebase' }) end, opts)
         vim.keymap.set('n', '<leader>gp', function() vim.cmd.Git('push') end, opts)
         vim.keymap.set('n', '<leader>gpn', '<cmd>Git push -u origin ', opts)
