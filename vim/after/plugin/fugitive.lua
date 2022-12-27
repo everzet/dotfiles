bc0f2e27a71506be30819a1510557c107e029a26
everzet <ever.zet@gmail.com>
2022-12-27 15:41:31 +0000

    add more quick keybinds for fugitive

--- vim/after/plugin/fugitive.lua
+++ vim/after/plugin/fugitive.lua
@@ -1,12 +1,15 @@
 vim.keymap.set('n', '<leader>gs', vim.cmd.Git, { remap = false })
 vim.keymap.set('n', '<leader>ga', vim.cmd.Gwrite, { remap = false })
-vim.keymap.set('n', '<leader>gc', function() vim.cmd.Git('commit') end, { remap = false })
+vim.keymap.set('n', '<leader>gd', vim.cmd.Gvdiffsplit, { remap = false })
+vim.keymap.set('n', '<leader>gco', vim.cmd.Gread, { remap = false })
+vim.keymap.set('n', '<leader>grn', vim.cmd.GRename, { remap = false })
+vim.keymap.set('n', '<leader>grm', vim.cmd.GDelete, { remap = false })
 
 local augroup = vim.api.nvim_create_augroup('Everzet_Fugitive', { clear = true })
 vim.api.nvim_create_autocmd('BufWinEnter', {
     group = augroup,
     callback = function()
-        if not vim.bo.filetype == 'fugitive' then return end
+        if vim.bo.filetype ~= 'fugitive' then return end
 
         local bufnr = vim.api.nvim_get_current_buf()
         local opts = { buffer = bufnr, remap = false }
@@ -14,6 +17,5 @@ vim.api.nvim_create_autocmd('BufWinEnter', {
         vim.keymap.set('n', '<leader>gP', function() vim.cmd.Git({ 'pull', '--rebase' }) end, opts)
         vim.keymap.set('n', '<leader>gp', function() vim.cmd.Git('push') end, opts)
         vim.keymap.set('n', '<leader>gpn', '<cmd>Git push -u origin ', opts)
-        vim.keymap.set('n', '<leader>gs', '<cmd>q<cr>', opts)
     end
 })
