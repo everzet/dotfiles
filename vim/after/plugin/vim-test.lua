f6a379d90935159684dc8b55d5d3efd52d3c46ee
everzet <konstantin.kudryashov@birdie.care>
2023-01-04 09:52:07 +0000

    add test runner
    we use Einar's fork of vim-test due to it properly supporting F#

---
+++ vim/after/plugin/vim-test.lua
@@ -0,0 +1,2 @@
+vim.keymap.set('n', '<leader>t', '<cmd>TestNearest<cr>', { remap = false })
+vim.keymap.set('n', '<leader>T', '<cmd>TestFile<cr>', { remap = false })
--- vim/lua/plugins.lua
+++ vim/lua/plugins.lua
@@ -1,8 +1,8 @@
 -- Install packer
 local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
-  vim.cmd [[packadd packer.nvim]]
+    vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
+    vim.cmd [[packadd packer.nvim]]
 end
 
 return require('packer').startup(function(use)
@@ -72,4 +72,7 @@ return require('packer').startup(function(use)
     use { 'nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' } }
     -- F# does not have TreeSitter (yet?), so we use normal vim plugin
     use 'kongo2002/fsharp-vim'
+
+    -- Running tests
+    use 'nordfjord/vim-test'
 end)
