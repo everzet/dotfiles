60c9e8a5ab93a40ba3701975fa19dbce485ac6c2
everzet <ever.zet@gmail.com>
2023-01-22 22:30:22 +0000

    adjust zk keybinds

--- vim/lua/plugins/wiki.lua
+++ vim/lua/plugins/wiki.lua
@@ -22,10 +22,10 @@ return {
             require('zk').setup(opts)
 
             local defaults = { noremap = true, silent = false }
-            vim.api.nvim_set_keymap("n", "<leader>ww", "<cmd>ZkNotes { sort = { 'modified' } }<cr>", defaults)
-            vim.api.nvim_set_keymap("n", "<leader>wt", "<cmd>ZkTags<cr>", defaults)
-            vim.api.nvim_set_keymap("n", "<leader>wn", "<cmd>ZkNew { title = vim.fn.input('Title: ') }<cr>", defaults)
-            vim.api.nvim_set_keymap("v", "<leader>wn", "<cmd>'<,'>ZkNewFromTitleSelection<cr>", defaults)
+            vim.api.nvim_set_keymap('n', '<leader>ww', '<cmd>ZkNotes { sort = { "modified" } }<cr>', defaults)
+            vim.api.nvim_set_keymap('n', '<leader>wt', '<cmd>ZkTags<cr>', defaults)
+            vim.api.nvim_set_keymap('n', '<leader>wn', '<cmd>ZkNew { title = vim.fn.input("Title: ") }<cr>', defaults)
+            vim.api.nvim_set_keymap('v', '<leader>wn', ':ZkNewFromTitleSelection<cr>', defaults)
         end
     }
 
