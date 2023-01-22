bb633b803a40167f0147ae64f6082f0dc9a372db
everzet <ever.zet@gmail.com>
2023-01-22 17:18:06 +0000

    do suggest LSP servers

--- vim/lua/plugins/language-servers.lua
+++ vim/lua/plugins/language-servers.lua
@@ -71,7 +71,7 @@ return {
 
             -- Override default preferences
             lsp.set_preferences({
-                suggest_lsp_servers = false
+                suggest_lsp_servers = true
             })
 
             -- Set keybinds on LSP attach to the buffer
