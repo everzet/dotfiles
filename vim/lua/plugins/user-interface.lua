c83eb7744254e5b13c6824e5b31530404f0b73d5
everzet <ever.zet@gmail.com>
2023-10-29 08:58:03 +0000

    move statusline into user-interface

--- vim/lua/plugins/user-interface.lua
+++ vim/lua/plugins/user-interface.lua
@@ -17,4 +17,55 @@ return {
             require("colors").inherit_hl("DiffDelete", "GitSignsDelete", { background = "none" })
         end,
     },
+
+    -- Statusline
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+            "nvim-lua/lsp-status.nvim",
+        },
+        config = function(_, _)
+            require("lualine").setup({
+                options = {
+                    theme = "iceberg_dark",
+                    icons_enabled = true,
+                    component_separators = "",
+                    section_separators = { left = "", right = "" },
+                    disabled_filetypes = {
+                        statusline = {},
+                        winbar = {},
+                    },
+                    ignore_focus = {},
+                    always_divide_middle = true,
+                    globalstatus = false,
+                    refresh = {
+                        statusline = 1000,
+                        tabline = 1000,
+                        winbar = 1000,
+                    },
+                },
+                sections = {
+                    lualine_a = { "mode" },
+                    lualine_b = { "branch", "diff" },
+                    lualine_c = { { "filetype", icon_only = true }, "filename" },
+                    lualine_x = { "diagnostics" },
+                    lualine_y = { "require'lsp-status'.status()" },
+                    lualine_z = { "searchcount", "location" },
+                },
+                inactive_sections = {
+                    lualine_a = {},
+                    lualine_b = {},
+                    lualine_c = { { "filetype", colored = false, icon_only = true }, "filename" },
+                    lualine_x = {},
+                    lualine_y = { "require'lsp-status'.status()" },
+                    lualine_z = {},
+                },
+                tabline = {},
+                winbar = {},
+                inactive_winbar = {},
+                extensions = {},
+            })
+        end,
+    },
 }
