d97826da5420af9d46045f5ba55dd18102df655a
everzet <ever.zet@gmail.com>
2023-10-29 12:55:40 +0000

    start adding descriptions to my commands

--- vim/lua/plugins/version-management.lua
+++ vim/lua/plugins/version-management.lua
@@ -5,7 +5,7 @@ return {
     {
         "mbbill/undotree",
         config = function(_, _)
-            vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+            vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = "Browse undo history" })
         end,
     },
 
@@ -13,13 +13,12 @@ return {
     {
         "tpope/vim-fugitive",
         config = function(_, _)
-            local defaults = { remap = true }
-            vim.keymap.set("n", "<leader>gs", vim.cmd.Git, defaults)
-            vim.keymap.set("n", "<leader>ga", vim.cmd.Gwrite, defaults)
-            vim.keymap.set("n", "<leader>gd", vim.cmd.Gvdiffsplit, defaults)
-            vim.keymap.set("n", "<leader>gco", vim.cmd.Gread, defaults)
-            vim.keymap.set("n", "<leader>grn", vim.cmd.GRename, defaults)
-            vim.keymap.set("n", "<leader>grm", vim.cmd.GDelete, defaults)
+            vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "Git status" })
+            vim.keymap.set("n", "<leader>ga", vim.cmd.Gwrite, { desc = "Stage current file" })
+            vim.keymap.set("n", "<leader>gd", vim.cmd.Gvdiffsplit, { desc = "Git diff current file" })
+            vim.keymap.set("n", "<leader>gco", vim.cmd.Gread, { desc = "Git checkout current file" })
+            vim.keymap.set("n", "<leader>grn", vim.cmd.GRename, { desc = "Git rename current file" })
+            vim.keymap.set("n", "<leader>grm", vim.cmd.GDelete, { desc = "Git delete current file" })
 
             local augroup = vim.api.nvim_create_augroup("Everzet_Fugitive", { clear = true })
             vim.api.nvim_create_autocmd("BufWinEnter", {
@@ -30,15 +29,13 @@ return {
                     end
 
                     local bufnr = vim.api.nvim_get_current_buf()
-                    local opts = { buffer = bufnr, remap = false }
 
                     vim.keymap.set("n", "<leader>gP", function()
                         vim.cmd.Git({ "pull", "--rebase" })
-                    end, opts)
+                    end, { buffer = bufnr, desc = "Git pull with rebase" })
                     vim.keymap.set("n", "<leader>gp", function()
                         vim.cmd.Git("push")
-                    end, opts)
-                    vim.keymap.set("n", "<leader>gpn", "<cmd>Git push -u origin ", opts)
+                    end, { buffer = bufnr, desc = "Git push" })
                 end,
             })
         end,
