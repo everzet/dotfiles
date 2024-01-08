3c8c2191e23381f335ad6f3ad034bd05fe1c38a9
everzet <konstantin.kudryashov@birdie.care>
2024-01-08 16:05:27 +0000

    add keybind for searching word under cursor

--- vim/lua/plugins/navigation.lua
+++ vim/lua/plugins/navigation.lua
@@ -38,11 +38,14 @@ return {
 
       -- Finders
       vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
-      vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch With [G]rep' })
       vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sb', function()
         require('telescope.builtin').buffers { grouped = true, initial_mode = 'normal' }
       end, { desc = '[S]earch Active [B]uffers' })
+
+      -- Grep
+      vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch With [G]rep' })
+      vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch [w]ord' })
 
       -- Lookup files under the neovim config folder
       vim.keymap.set('n', '<leader>sc', function()
@@ -71,5 +74,4 @@ return {
       end, { desc = 'Browse [F]iles From [P]roject Root' })
     end,
   },
-
 }
