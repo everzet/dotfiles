2257927698d1a5488c59654125559cde82ca5ed2
everzet <ever.zet@gmail.com>
2024-04-01 15:11:05 +0100

    switch Go version automatically with ASDF

--- fish/conf.d/envars.fish
+++ fish/conf.d/envars.fish
@@ -11,5 +11,8 @@ set -gx CLICOLOR_FORCE true
 set -gx LANG "en_GB.UTF-8"
 set -gx LC_CTYPE "en_GB.UTF-8"
 
+# asdf
+set -gx ASDF_GOLANG_MOD_VERSION_ENABLED true
+
 # zk wiki
 set -gx ZK_NOTEBOOK_DIR "$HOME/Library/CloudStorage/Dropbox/Wiki"
