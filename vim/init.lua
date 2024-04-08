512bf9f47c70df432b6e569255e1e47493926f9f
everzet <konstantin.kudryashov@birdie.care>
2024-04-08 22:52:04 +0100

    install linters for go

--- vim/init.lua
+++ vim/init.lua
@@ -523,6 +523,26 @@ require('lazy').setup {
     end,
   },
 
+  { -- Linting
+    'mfussenegger/nvim-lint',
+    event = { 'BufReadPre', 'BufNewFile' },
+    config = function()
+      local lint = require 'lint'
+
+      lint.linters_by_ft = {
+        go = { 'golangcilint' },
+      }
+
+      local augroup = vim.api.nvim_create_augroup('kickstart-nvim-lint', { clear = true })
+      vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
+        group = augroup,
+        callback = function()
+          require('lint').try_lint()
+        end,
+      })
+    end,
+  },
+
   { -- Autoformat
     'stevearc/conform.nvim',
     opts = {
