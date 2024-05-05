e8923d259b3c327ed1813ad026a10922a2fe6b2c
everzet <ever.zet@gmail.com>
2024-05-05 11:04:11 +0100

    fine-tune diagnostics icons

--- vim/init.lua
+++ vim/init.lua
@@ -146,9 +146,9 @@ vim.diagnostic.config {
 }
 
 -- Use icons as diagnostic signs
-vim.fn.sign_define('DiagnosticSignError', { text = '', texthl = 'DiagnosticSignError', linehl = '', numhl = '' })
-vim.fn.sign_define('DiagnosticSignWarn', { text = '', texthl = 'DiagnosticSignWarn', linehl = '', numhl = '' })
-vim.fn.sign_define('DiagnosticSignInfo', { text = '', texthl = 'DiagnosticSignInfo', linehl = '', numhl = '' })
+vim.fn.sign_define('DiagnosticSignError', { text = '', texthl = 'DiagnosticSignError', linehl = '', numhl = '' })
+vim.fn.sign_define('DiagnosticSignWarn', { text = '󰀦', texthl = 'DiagnosticSignWarn', linehl = '', numhl = '' })
+vim.fn.sign_define('DiagnosticSignInfo', { text = '', texthl = 'DiagnosticSignInfo', linehl = '', numhl = '' })
 vim.fn.sign_define('DiagnosticSignHint', { text = '', texthl = 'DiagnosticSignHint', linehl = '', numhl = '' })
 
 -- [[ Configure and install plugins ]]
