7adb9e538ef443956aaf9af7d921c2edd79f1cab
everzet <ever.zet@gmail.com>
2024-09-04 15:34:04 +0100

    fine-tune IEx defaults

--- elixir/iex.exs
+++ elixir/iex.exs
@@ -1,11 +1,41 @@
-IEx.configure(colors: [enabled: true],
-              default_prompt: [
-                "\e[G",
-                "\e[3m\e[1m",
-                "%prefix(",
-                "\e[0m\e[1m",
-                "%counter",
-                "\e[3m\e[1m",
-                ")>",
-                "\e[0m"
-              ] |> IO.chardata_to_string())
+local_time = fn ->
+  {_date, {hour, minute, _second}} = :calendar.local_time()
+
+  [hour, minute]
+  |> Enum.map(&String.pad_leading(Integer.to_string(&1), 2, "0"))
+  |> Enum.join(":")
+end
+
+IEx.configure(
+  colors: [
+    syntax_colors: [
+      number: :light_yellow,
+      atom: :light_cyan,
+      string: :light_black,
+      boolean: :red,
+      nil: [:magenta, :bright]
+    ],
+    ls_directory: :cyan,
+    ls_device: :yellow,
+    doc_code: :green,
+    doc_inline_code: :magenta,
+    doc_headings: [:cyan, :underline],
+    doc_title: [:cyan, :bright, :underline]
+  ],
+  default_prompt:
+    "#{IO.ANSI.green()}%prefix#{IO.ANSI.reset()} " <>
+      "[#{IO.ANSI.magenta()}#{local_time.()}#{IO.ANSI.reset()} " <>
+      ":: #{IO.ANSI.cyan()}%counter#{IO.ANSI.reset()}] >",
+  alive_prompt:
+    "#{IO.ANSI.green()}%prefix#{IO.ANSI.reset()} " <>
+      "(#{IO.ANSI.yellow()}%node#{IO.ANSI.reset()}) " <>
+      "[#{IO.ANSI.magenta()}#{local_time.()}#{IO.ANSI.reset()} " <>
+      ":: #{IO.ANSI.cyan()}%counter#{IO.ANSI.reset()}] >",
+  history_size: 50,
+  inspect: [
+    pretty: true,
+    limit: :infinity,
+    width: 80
+  ],
+  width: 80
+)
