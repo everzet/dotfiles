275ac5400e991241bbaa9c7bbefd2daee4e443bd
everzet <ever.zet@gmail.com>
2024-09-06 15:51:02 +0100

    adjust fish aliases

--- fish/conf.d/abbrs.fish
+++ fish/conf.d/abbrs.fish
@@ -1,7 +1,12 @@
-# General
+# Listing
 abbr l ls
+abbr la ls -a
 abbr lt ls -T
+
+# Vim
 abbr v vim
+
+# Git
 abbr g git
 
 # Zellij
--- fish/functions/ls.fish
+++ fish/functions/ls.fish
@@ -1,3 +1,3 @@
 function ls --wraps eza --description "list directory contents"
-    eza --long --group-directories-first --time-style 'relative' $argv;
+    eza --long --group-directories-first --icons=always --time-style 'relative' $argv;
 end
