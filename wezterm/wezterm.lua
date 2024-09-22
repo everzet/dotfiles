8fc301fb58b7a95a3bab1b31e38ef9b2561c9cd2
everzet <ever.zet@gmail.com>
2024-09-22 11:18:14 +0100

    configure wezterm copy modes

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
--- Set default shell to Fish
+-- Attach to or create a Zellij scratch session on start
 config.default_prog = {
   "/opt/homebrew/bin/zellij",
   "--layout",
@@ -15,13 +15,34 @@ config.default_prog = {
 config.font = wezterm.font("MonoLisa Nerd Font", { weight = "Medium" })
 config.freetype_load_target = "Light"
 config.font_size = 12.0
-config.line_height = 1.4
+config.line_height = 1.5
+
+-- Configure colors
+config.color_scheme = "Tokyo Night (Gogh)"
+config.colors = {
+  selection_fg = "white",
+  selection_bg = "red",
+}
 
 -- Configure visuals
-config.color_scheme = "Tokyo Night"
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
 config.window_padding = { left = 30, right = 30, top = 25, bottom = 25 }
+config.window_background_opacity = 0.95
 config.window_close_confirmation = "NeverPrompt"
+
+-- Configure keybinds
+config.keys = {
+  {
+    key = "Backspace",
+    mods = "CTRL",
+    action = wezterm.action.QuickSelect,
+  },
+  {
+    key = "Backspace",
+    mods = "CTRL|SHIFT",
+    action = wezterm.action.ActivateCopyMode,
+  },
+}
 
 return config
