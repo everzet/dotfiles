9368513c459fa6e7257a694c68a63550217439e9
kk <konstantin@v7labs.com>
2024-10-09 09:47:12 +0100

    disable wait time adjustment

--- vim/init.lua
+++ vim/init.lua
@@ -37,10 +37,6 @@ vim.opt.signcolumn = 'yes'
 -- Decrease update time
 vim.opt.updatetime = 250
 
--- Decrease mapped sequence wait time
--- Displays which-key popup sooner
-vim.opt.timeoutlen = 300
-
 -- Configure how new splits should be opened
 vim.opt.splitright = true
 vim.opt.splitbelow = true
