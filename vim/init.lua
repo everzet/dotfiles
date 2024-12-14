66d5977e2b02e659b2c940b14fdf814cba9da01d
Konstantin Kudryashov <ever.zet@gmail.com>
2024-12-14 15:48:18 +0000

    do not split to show partial search results

--- vim/init.lua
+++ vim/init.lua
@@ -48,7 +48,7 @@ vim.opt.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
 -- Preview substitutions live, as you type!
-vim.opt.inccommand = 'split'
+vim.opt.inccommand = 'nosplit'
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
