3aed247e129e960f98ac305bcfe30c5ba5f6b820
Konstantin Kudryashov <ever.zet@gmail.com>
2024-12-18 14:54:10 +0000

    swtich from nvim-cmp to blink.cmp

--- vim/lua/plugins/lsp.lua
+++ vim/lua/plugins/lsp.lua
@@ -2,6 +2,9 @@ return {
   { -- LSP Configuration & Plugins
     'neovim/nvim-lspconfig',
     dependencies = {
+      -- Make sure autocompleter is installed
+      'saghen/blink.cmp',
+
       -- Automatically install LSPs and related tools to stdpath for neovim
       'williamboman/mason.nvim',
       'williamboman/mason-lspconfig.nvim',
@@ -113,7 +116,7 @@ return {
       --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
       --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
       local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
+      capabilities = vim.tbl_deep_extend('force', capabilities, require('blink.cmp').get_lsp_capabilities())
 
       -- Enable the following language servers
       --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
