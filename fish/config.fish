67b98ebb885b5d8176f9fa1d1f1963974e4fba41
Konstantin Kudryashov <ever.zet@gmail.com>
2024-12-18 14:57:28 +0000

    move fish keybinds back to the main config
    cursor gets screwy without that

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,10 @@
 if status is-interactive
-    # Keybinds
-    setup_keybinds
+    # Vi keybinds
+    fish_vi_key_bindings
+    set fish_vi_force_cursor true
+    set fish_cursor_default block
+    set fish_cursor_insert line
+    set fish_cursor_replace_one underscore
 
     # Custom fish theme
     setup_theme
--- fish/functions/setup_keybinds.fish
+++
@@ -1,7 +0,0 @@
-function setup_keybinds
-    fish_vi_key_bindings
-    set fish_vi_force_cursor true
-    set fish_cursor_default block
-    set fish_cursor_insert line
-    set fish_cursor_replace_one underscore
-end
