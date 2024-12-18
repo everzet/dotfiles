44a9413fd5c16b8f29b4f4177f1df074b7823163
Konstantin Kudryashov <ever.zet@gmail.com>
2024-12-18 14:42:31 +0000

    setup Tokyo Night theme across all tools

--- fish/config.fish
+++ fish/config.fish
@@ -1,15 +1,12 @@
 if status is-interactive
-    # configure vim keybinds
-    fish_vi_key_bindings
-    set fish_vi_force_cursor true
-    set fish_cursor_default block
-    set fish_cursor_insert line
-    set fish_cursor_replace_one underscore
+    # Keybinds
+    setup_keybinds
 
-    # Fuzzy file finder
-    register_fzf_keybinds
-    set -gx FZF_DEFAULT_COMMAND 'rg --files --ignore --hidden --follow --glob "!.git/*"'
-    set -gx FZF_CTRL_T_COMMAND 'rg --files --ignore --hidden --follow --glob "!.git/*"'
+    # Custom fish theme
+    setup_theme
+
+    # Interactive file finder
+    setup_fzf
 
     # Neat and fully-fledged prompt
     starship init fish | source
