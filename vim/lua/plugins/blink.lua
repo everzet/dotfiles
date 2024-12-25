8b8b5c5bea1d3f91acf13e721593885b810a6773
Konstantin Kudryashov <ever.zet@gmail.com>
2024-12-25 14:20:47 +0000

    disable autocomplete in cmdline

--- vim/lua/plugins/blink.lua
+++ vim/lua/plugins/blink.lua
@@ -6,12 +6,15 @@ return {
     opts = {
       keymap = { preset = 'default' },
 
+      sources = {
+        -- Disable cmdline completions
+        cmdline = {},
+      },
+
       appearance = {
         use_nvim_cmp_as_default = true,
         nerd_font_variant = 'mono',
       },
-
-      signature = { enabled = true },
 
       completion = {
         documentation = {
@@ -19,6 +22,8 @@ return {
           window = { border = 'rounded' },
         },
       },
+
+      signature = { enabled = true },
     },
   },
 }
