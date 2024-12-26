e7cc7d35ef0e29d2e540c7ca57bceda29a3d9d1c
Konstantin Kudryashov <ever.zet@gmail.com>
2024-12-26 13:20:55 +0000

    drop git plugins as I am planning to switch to jj

--- nvim/lua/plugins/fugitive.lua
+++
@@ -1,9 +0,0 @@
-return {
-  { -- Git management
-    'tpope/vim-fugitive',
-    config = function()
-      vim.keymap.set('n', '<leader>gs', vim.cmd.Git, { desc = '[G]it [S]tatus' })
-      vim.keymap.set('n', '<leader>ga', vim.cmd.Gwrite, { desc = '[G]it [A]dd' })
-    end,
-  },
-}
--- nvim/lua/plugins/gitsigns.lua
+++
@@ -1,3 +0,0 @@
-return {
-  { 'lewis6991/gitsigns.nvim', opts = {} }, -- Adds git related signs to the gutter
-}
