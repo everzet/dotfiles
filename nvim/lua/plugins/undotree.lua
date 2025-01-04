14b4e9c0017e49873b723846c0e82261fa7bf93a
Konstantin Kudryashov <ever.zet@gmail.com>
2025-01-04 09:33:03 +0000

    get rid of unused vim plugins

--- nvim/lua/plugins/undotree.lua
+++
@@ -1,9 +0,0 @@
-return {
-  { -- Visual undo for times when we really need to go back
-    'mbbill/undotree',
-    event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-    config = function(_, _)
-      vim.keymap.set('n', '<leader>cu', vim.cmd.UndotreeToggle, { desc = '[C]code [U]ndo' })
-    end,
-  },
-}
